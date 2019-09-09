ca508e274560191c44d17b22ef478808b6e7c208
Jay Hankins <hankins.jay@gmail.com>
2019-09-08 17:10:03 -0700

    no journ

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,7 @@ module.exports = {
         short_name: `jblog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#222`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
