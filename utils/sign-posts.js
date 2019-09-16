f4d1884d82d0b0cd080eca89a4a8f566ed0399d6
Jay Hankins <hankins.jay@gmail.com>
2019-09-15 22:04:48 -0700

    signing journals

--- utils/sign-posts.js
+++ utils/sign-posts.js
@@ -1,11 +1,18 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+exports.__esModule = true;
 var fs = require("file-system");
 var child_process_1 = require("child_process");
 function spawnGPG(filePath) {
     console.log("Signing " + filePath);
     var options = ['--armor', '--batch', '--sign'];
-    var childProc = child_process_1.spawn('gpg', options.concat([filePath]));
+    var childProc = child_process_1.spawn('gpg', __spreadArrays(options, [filePath]));
     childProc.on('exit', function (code) {
         code !== 0 ? console.log("GPG error code: " + code) : null;
     });
@@ -25,3 +32,4 @@ function signFolder(folderPath) {
     });
 }
 signFolder('content/blog');
+signFolder('content/journal');
